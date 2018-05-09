c26fb5e1945c27f401c9eca81c82bf1dfd7c9a23
JJ <mrcoldean@gmail.com>
2018-05-09 15:07:38 +0200

    Menü verändert

--- app/src/main/java/com/example/jale/poi_app/Search.java
+++ app/src/main/java/com/example/jale/poi_app/Search.java
@@ -82,7 +82,7 @@ public class Search extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.favoriten) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
