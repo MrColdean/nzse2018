63b994003db8067fddf7d4acc466cf8473bb10ef
JJ <mrcoldean@gmail.com>
2018-05-23 17:15:13 +0200

    ListView in result eingearbeitet

--- app/src/main/java/com/example/jale/poi_app/ResultFragment.java
+++ app/src/main/java/com/example/jale/poi_app/ResultFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -22,10 +24,19 @@ public class ResultFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_result, container, false);
         // Inflate the layout for this fragment
+        String [] menueItems = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s"};
+        ListView listView = view.findViewById(R.id.listviewresult);
+        ArrayAdapter<String> listviewadapter = new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_1,menueItems);
+listView.setAdapter(listviewadapter);
         getActivity().setTitle(getString(R.string.ergebnis));
 
-        return inflater.inflate(R.layout.fragment_result, container, false);
+
+
+
+        return view;
     }
 
 }
